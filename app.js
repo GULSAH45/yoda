11e740be2623bc0e07157eba794da3731ea31937
GULSAH45 <178339362+GULSAH45@users.noreply.github.com>
2024-12-03 23:54:21 +0300

    done

--- app.js
+++ app.js
@@ -1,157 +1,157 @@
-let characters = [{
-  "id": 1,
-  "name": "Luke Skywalker",
-  "pic": "https://vignette.wikia.nocookie.net/starwars/images/2/20/LukeTLJ.jpg",
-  "homeworld": "tatooine"
-},
-{
-  "id": 2,
-  "name": "C-3PO",
-  "pic": "https://vignette.wikia.nocookie.net/starwars/images/3/3f/C-3PO_TLJ_Card_Trader_Award_Card.png",
-  "homeworld": "tatooine"
-},
-{
-  "id": 3,
-  "name": "R2-D2",
-  "pic": "https://vignette.wikia.nocookie.net/starwars/images/e/eb/ArtooTFA2-Fathead.png",
-  "homeworld": "naboo"
-},
-{
-  "id": 4,
-  "name": "Darth Vader",
-  "pic": "https://vignette.wikia.nocookie.net/fr.starwars/images/3/32/Dark_Vador.jpg",
-  "homeworld": "tatooine"
-},
-{
-  "id": 5,
-  "name": "Leia Organa",
-  "pic": "https://vignette.wikia.nocookie.net/starwars/images/f/fc/Leia_Organa_TLJ.png",
-  "homeworld": "alderaan"
-},
-{
-  "id": 6,
-  "name": "Owen Lars",
-  "pic": "https://vignette.wikia.nocookie.net/starwars/images/e/eb/OwenCardTrader.png",
-  "homeworld": "tatooine"
-},
-{
-  "id": 7,
-  "name": "Beru Whitesun lars",
-  "pic": "https://vignette.wikia.nocookie.net/starwars/images/c/cc/BeruCardTrader.png",
-  "homeworld": "tatooine"
-},
-{
-  "id": 8,
-  "name": "R5-D4",
-  "pic": "https://vignette.wikia.nocookie.net/starwars/images/c/cb/R5-D4_Sideshow.png",
-  "homeworld": "tatooine"
-},
-{
-  "id": 9,
-  "name": "Biggs Darklighter",
-  "pic": "https://vignette.wikia.nocookie.net/starwars/images/0/00/BiggsHS-ANH.png",
-  "homeworld": "tatooine"
-},
-{
-  "id": 10,
-  "name": "Obi-Wan Kenobi",
-  "pic": "https://vignette.wikia.nocookie.net/starwars/images/4/4e/ObiWanHS-SWE.jpg",
-  "homeworld": "stewjon"
-},
-{
-  "id": 11,
-  "name": "Anakin Skywalker",
-  "pic": "https://vignette.wikia.nocookie.net/starwars/images/6/6f/Anakin_Skywalker_RotS.png",
-  "homeworld": "tatooine"
-},
-{
-  "id": 12,
-  "name": "Wilhuff Tarkin",
-  "pic": "https://vignette.wikia.nocookie.net/starwars/images/c/c1/Tarkininfobox.jpg",
-  "homeworld": "eriadu"
-},
-{
-  "id": 13,
-  "name": "Chewbacca",
-  "pic": "https://vignette.wikia.nocookie.net/starwars/images/4/48/Chewbacca_TLJ.png",
-  "homeworld": "kashyyyk"
-},
-{
-  "id": 14,
-  "name": "Han Solo",
-  "pic": "https://vignette.wikia.nocookie.net/starwars/images/e/e2/TFAHanSolo.png",
-  "homeworld": "corellia"
-},
-{
-  "id": 15,
-  "name": "Greedo",
-  "pic": "https://vignette.wikia.nocookie.net/starwars/images/c/c6/Greedo.jpg",
-  "homeworld": "Rodia"
-},
-{
-  "id": 16,
-  "name": "Jabba Desilijic Tiure",
-  "pic": "https://vignette.wikia.nocookie.net/starwars/images/7/7f/Jabba_SWSB.png",
-  "homeworld": "tatooine"
-},
-{
-  "id": 18,
-  "name": "Wedge Antilles",
-  "pic": "https://vignette.wikia.nocookie.net/starwars/images/6/60/WedgeHelmetless-ROTJHD.jpg",
-  "homeworld": "corellia"
-},
-{
-  "id": 19,
-  "name": "Jek Tono Porkins",
-  "pic": "https://vignette.wikia.nocookie.net/starwars/images/e/eb/JekPorkins-DB.png",
-  "homeworld": "bestine"
-},
-{
-  "id": 20,
-  "name": "Yoda",
-  "pic": "https://vignette.wikia.nocookie.net/starwars/images/d/d6/Yoda_SWSB.png"
-},
-{
-  "id": 21,
-  "name": "Palpatine",
-  "pic": "https://vignette.wikia.nocookie.net/starwars/images/d/d8/Emperor_Sidious.png",
-  "homeworld": "naboo"
-}]
+let characters = [
+  {
+    id: 1,
+    name: "Luke Skywalker",
+    pic: "https://vignette.wikia.nocookie.net/starwars/images/2/20/LukeTLJ.jpg",
+    homeworld: "tatooine",
+  },
+  {
+    id: 2,
+    name: "C-3PO",
+    pic: "https://vignette.wikia.nocookie.net/starwars/images/3/3f/C-3PO_TLJ_Card_Trader_Award_Card.png",
+    homeworld: "tatooine",
+  },
+  {
+    id: 3,
+    name: "R2-D2",
+    pic: "https://vignette.wikia.nocookie.net/starwars/images/e/eb/ArtooTFA2-Fathead.png",
+    homeworld: "naboo",
+  },
+  {
+    id: 4,
+    name: "Darth Vader",
+    pic: "https://vignette.wikia.nocookie.net/fr.starwars/images/3/32/Dark_Vador.jpg",
+    homeworld: "tatooine",
+  },
+  {
+    id: 5,
+    name: "Leia Organa",
+    pic: "https://vignette.wikia.nocookie.net/starwars/images/f/fc/Leia_Organa_TLJ.png",
+    homeworld: "alderaan",
+  },
+  {
+    id: 6,
+    name: "Owen Lars",
+    pic: "https://vignette.wikia.nocookie.net/starwars/images/e/eb/OwenCardTrader.png",
+    homeworld: "tatooine",
+  },
+  {
+    id: 7,
+    name: "Beru Whitesun lars",
+    pic: "https://vignette.wikia.nocookie.net/starwars/images/c/cc/BeruCardTrader.png",
+    homeworld: "tatooine",
+  },
+  {
+    id: 8,
+    name: "R5-D4",
+    pic: "https://vignette.wikia.nocookie.net/starwars/images/c/cb/R5-D4_Sideshow.png",
+    homeworld: "tatooine",
+  },
+  {
+    id: 9,
+    name: "Biggs Darklighter",
+    pic: "https://vignette.wikia.nocookie.net/starwars/images/0/00/BiggsHS-ANH.png",
+    homeworld: "tatooine",
+  },
+  {
+    id: 10,
+    name: "Obi-Wan Kenobi",
+    pic: "https://vignette.wikia.nocookie.net/starwars/images/4/4e/ObiWanHS-SWE.jpg",
+    homeworld: "stewjon",
+  },
+  {
+    id: 11,
+    name: "Anakin Skywalker",
+    pic: "https://vignette.wikia.nocookie.net/starwars/images/6/6f/Anakin_Skywalker_RotS.png",
+    homeworld: "tatooine",
+  },
+  {
+    id: 12,
+    name: "Wilhuff Tarkin",
+    pic: "https://vignette.wikia.nocookie.net/starwars/images/c/c1/Tarkininfobox.jpg",
+    homeworld: "eriadu",
+  },
+  {
+    id: 13,
+    name: "Chewbacca",
+    pic: "https://vignette.wikia.nocookie.net/starwars/images/4/48/Chewbacca_TLJ.png",
+    homeworld: "kashyyyk",
+  },
+  {
+    id: 14,
+    name: "Han Solo",
+    pic: "https://vignette.wikia.nocookie.net/starwars/images/e/e2/TFAHanSolo.png",
+    homeworld: "corellia",
+  },
+  {
+    id: 15,
+    name: "Greedo",
+    pic: "https://vignette.wikia.nocookie.net/starwars/images/c/c6/Greedo.jpg",
+    homeworld: "Rodia",
+  },
+  {
+    id: 16,
+    name: "Jabba Desilijic Tiure",
+    pic: "https://vignette.wikia.nocookie.net/starwars/images/7/7f/Jabba_SWSB.png",
+    homeworld: "tatooine",
+  },
+  {
+    id: 18,
+    name: "Wedge Antilles",
+    pic: "https://vignette.wikia.nocookie.net/starwars/images/6/60/WedgeHelmetless-ROTJHD.jpg",
+    homeworld: "corellia",
+  },
+  {
+    id: 19,
+    name: "Jek Tono Porkins",
+    pic: "https://vignette.wikia.nocookie.net/starwars/images/e/eb/JekPorkins-DB.png",
+    homeworld: "bestine",
+  },
+  {
+    id: 20,
+    name: "Yoda",
+    pic: "https://vignette.wikia.nocookie.net/starwars/images/d/d6/Yoda_SWSB.png",
+  },
+  {
+    id: 21,
+    name: "Palpatine",
+    pic: "https://vignette.wikia.nocookie.net/starwars/images/d/d8/Emperor_Sidious.png",
+    homeworld: "naboo",
+  },
+];
 
 // karakterler kapalı gelsin diye
 
 let characterRender = false;
-const characterRow = document.getElementById("characterRow")
-const toggleButton = document.getElementById("toggleBtn")
-const radioInput = document.getElementById("RadioInput")
+const characterRow = document.getElementById("characterRow");
+const toggleButton = document.getElementById("toggleBtn");
+const radioInput = document.getElementById("RadioInput");
 
-// kapalı gelen şeye bastık açıldı 
+// kapalı gelen şeye bastık açıldı
 
-function toggleRender(){
-  if (characterRender){
-    characterRender = false
+function toggleRender() {
+  if (characterRender) {
+    characterRender = false;
 
-//  karakter satırını boşalttık
-    
-characterRow.innerHTML = ""
+    //  karakter satırını boşalttık
 
-toggleButton.textContent = "SHOW"
+    characterRow.innerHTML = "";
 
-// açıkken bastık kapandı
+    toggleButton.textContent = "SHOW";
 
+    // açıkken bastık kapandı
   } else {
-    characters.map((armut)=> { 
-      characterRow.innerHTML +=  
-      `<div class="col-lg-4 col-sm-6 mb-5" id="filterDiv" data-id="${armut.homeworld}">
-            <div class="card bg-black fw-bold text-light border-light" style="width: 18rem;">
-              <img src="${armut.pic}" class="card-img-top" alt="..." style="height: 350px;">
+    characters.map((armut) => {
+      characterRow.innerHTML += `<div class="justify-content-center col-xl-4 col-lg-4 col-sm-6 mt-5" id="filterDiv" data-id="${armut.homeworld}">
+            <div class=" card fw-bold text-light border-light" style="width: 18rem;">
+              <img src="${armut.pic}" class="card-img-top object-fit-contain" alt="..." style="height: 350px;">
               <div class="card-body">
                 <p class="card-text">${armut.name}</p>
                 <p class="card-text">${armut.homeworld}</p>
               </div>
             </div>
-          </div>`
-    })
+          </div>`;
+    });
     characterRender = true;
     toggleButton.textContent = "HIDE";
   }
@@ -159,23 +159,20 @@ toggleButton.textContent = "SHOW"
 
 // burdan sonrası radio
 
-
-
 const characterMemleket = characters.map((memleket) => {
-  return memleket.homeworld
+  return memleket.homeworld;
 });
 
-const uniqueMemleket = [...new Set(characterMemleket)]
-function createInput(){
+const uniqueMemleket = [...new Set(characterMemleket)];
+function createInput() {
   characterRow.innerHTML = "";
-  uniqueMemleket.map((homeworld) => {`   <div class="form-check mb-4 col-lg-3 col-md-6 col-sm-12">
+  uniqueMemleket.map((homeworld) => {
+    `   <div class="form-check mb-4 col-lg-3 col-md-6 col-sm-12">
           <input type="radio" class="form-check-input" value="${homeworld}" name="exampleRadioInput">
           <label for="exampleRadioInput" class="form-check-label text-white fs-6">${homeworld}</label>
-        </div>`
-    
-  })
+        </div>`;
+  });
 }
-
 
 // let filteredHomeworld
 //   function filterCharacters(homeworld) {
@@ -186,7 +183,7 @@ function createInput(){
 //       (
 //         armut
 //       ) => ` <div class="col-lg-4 col-sm-6 mb-5" id="filterDiv" data-id="${armut.homeworld}">
-//             <div class="card bg-black 
+//             <div class="card bg-black
 //            fw-bold text-warning border-light" style="width: 50rem;">
 //               <img src="${armut.pic}" class="card-img-top" alt="..." style="height: 350px;">
 //               <div class="card-body">
